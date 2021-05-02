ce6174160d2920827d6cb1c959796d2856b1eda5
hsiung <vjoke@foxmail.com>
2021-05-02 12:25:05 +0800

    FIX: buy group is empty even chase_up is on

--- venus/pkg/plutus/oracle.go
+++ venus/pkg/plutus/oracle.go
@@ -17,7 +17,7 @@ type Oracle struct {
 	buy_threshold  float64
 	sell_threshold float64
 	sell_on_fail   bool
-	chase_up	   bool
+	chase_up       bool
 	epochMap       map[string]*model.Epoch
 	tick           uint64
 	tickCount      uint64
@@ -30,8 +30,8 @@ func NewOracle(arb *Arbitrager) *Oracle {
 		slideDetect:    arb.config.Policy.Sample.SlideDetect,
 		buy_threshold:  arb.config.Policy.Trigger.BuyThreshold,
 		sell_threshold: arb.config.Policy.Trigger.SellThreshold,
-		sell_on_fail: 	arb.config.Policy.Trade.SellOnFall,
-		chase_up: 		arb.config.Policy.Trade.ChaseUp,
+		sell_on_fail:   arb.config.Policy.Trade.SellOnFall,
+		chase_up:       arb.config.Policy.Trade.ChaseUp,
 		epochMap:       make(map[string]*model.Epoch),
 		symbols:        arb.config.Policy.Symbols,
 	}
@@ -101,18 +101,18 @@ func (o *Oracle) Run(stopCh <-chan struct{}) {
 						sellGroup := append(fallGroup, otherGroup...)
 						o.arb.CreateOrders(&model.Order{Type: model.SELL_ORDER, Symbols: sellGroup})
 					} else {
-						oLog.Warnf("do not sell because sell_on_fail is disabled!!!")
+						oLog.Warnf("do not sell because sell_on_fall is disabled!!!")
 					}
 				} else if float64(len(riseGroup))/float64(o.symbolsLen) >= o.buy_threshold {
 					oLog.Infof("trigger buy orders with rise:%v/all:%v, threshod %v", len(riseGroup), o.symbolsLen, o.buy_threshold)
-					if len(otherGroup) == 0 {
-						oLog.Warnf("empty group for orders, do nothing!")
+					buyGroup := otherGroup
+					if o.chase_up {
+						// FIXME: riseGroup first
+						buyGroup = append(riseGroup, buyGroup...)
+					}
+					if len(buyGroup) == 0 {
+						oLog.Warnf("empty buy group for orders, do nothing!")
 					} else {
-						buyGroup := otherGroup
-						if o.chase_up {
-							// FIXME: riseGroup first
-							buyGroup = append(riseGroup, buyGroup...)
-						}	
 						o.arb.CreateOrders(&model.Order{Type: model.BUY_ORDER, Symbols: buyGroup})
 					}
 				} else {
